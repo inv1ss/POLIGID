7cf5dfe50d431a29e362eab1c8de96a17c4dc5f9
VolodymyrOn <saharokword@gmail.com>
2023-02-16 00:41:07 +0200

    search xml v2.0

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingM : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.search)
+        bindingM= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingM.root)
     }
-}
+
+    fun search(){
+      //  bindingM.searchb.setOnClickListener(){
+            //val intent = Intent(Search1Activity::class.java)
+
+        }
+    }
