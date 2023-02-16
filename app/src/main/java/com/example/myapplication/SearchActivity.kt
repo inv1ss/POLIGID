d977f24806702813c2da0162116306bd3f64d65e
VolodymyrOn <saharokword@gmail.com>
2023-02-16 17:06:22 +0200

    recyclerview adapter2

--- app/src/main/java/com/example/myapplication/SearchActivity.kt
+++ app/src/main/java/com/example/myapplication/SearchActivity.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
     lateinit var bindingS : ActivitySearchBinding
+    private val adapter = SearchAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingS= ActivitySearchBinding.inflate(layoutInflater)
         setContentView(bindingS.root)
+    }
+
+    private fun init() {
+        bindingS.apply{
+            recycleView.layoutManager=GridLayoutManager(this@SearchActivity, 1)
+            recycleView.adapter = adapter
+        }
     }
 
 
